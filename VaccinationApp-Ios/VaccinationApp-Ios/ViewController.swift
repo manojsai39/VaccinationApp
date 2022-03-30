e0130e6947a8e039045c53f2954c0b9c4ee0d3bb
Manpreet636 <75639402+Manpreet636@users.noreply.github.com>
2022-03-30 15:50:54 -0400

    EmptyProject

---
+++ VaccinationApp-Ios/VaccinationApp-Ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VaccinationApp-Ios
+//
+//  Created by Manpreet Kaur on 2022-03-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
